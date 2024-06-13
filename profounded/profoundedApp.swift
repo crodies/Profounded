433702b0edcd03e7f812b3f819ec68401bba64f9
Vansh <patelvansh1023@gmail.com>
2024-06-13 11:49:10 -0400

    Initial Commit

---
+++ profounded/profoundedApp.swift
@@ -0,0 +1,17 @@
+//
+//  profoundedApp.swift
+//  profounded
+//
+//  Created by Vansh Patel on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct profoundedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
